f6318575192c74c2895bc39ef1d0fffbe3ced081
vicky <143078285+Mr-Vicky-01@users.noreply.github.com>
2024-01-18 23:48:17 +0530

    Update server.py

--- server.py
+++ server.py
@@ -40,7 +40,7 @@ templates1 = Jinja2Templates(directory="models")
 # DL or Ml Models (Loading)..
 sports_ball_model = tf.keras.models.load_model('models/sports_ball/Sports_ball_prediction_v2.h5')
 weather_model = tf.keras.models.load_model('models/weather/weather_prediction_v2.h5')
-# flower_model = tf.keras.models.load_model('models/flower/flower_prediction.h5')
+flower_model = tf.keras.models.load_model('models/yoga_pose/yoga-modelv2.h5')
 yoga_pose_model = tf.keras.models.load_model('models/yoga_pose/yoga-modelv2.h5')
 mammals_model = tf.keras.models.load_model('models/mammals/Mammals_predictionv1.h5')
 card_model = tf.keras.models.load_model('models/card/card_model_v2.h5')
